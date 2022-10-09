5d5f05a15f519801e13f8f66e830314bf9986934
M Faisal Hayat <m.faisalhayat3@gmail.com>
2022-10-09 11:52:52 +0500

    now k-menas and knn are working properly

--- Dr Umer Research.py
+++ Dr Umer Research.py
@@ -1,21 +1,14 @@
-# -*- coding: utf-8 -*-
-
-# -- Sheet --
-
-# # K-Means 
-# ## Apply K-Means on Dataset and Make Predictions
-
-
+##
 import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-sns.set()  # Set it for plotting
+# sns.set()  # Set it for plotting
 print(f'numpy version is : {np.__version__}')
 print(f'pandas version is : {pd.__version__}')
 print(f'seaborn version is : {sns.__version__}')
-
+##
 from sklearn.datasets import make_blobs
 
 X, y_true = make_blobs(n_samples=1000, centers=4,
@@ -24,7 +17,8 @@ plt.scatter(X[:, 0], X[:, 1], s=50)
 plt.title('Displaying Data')
 plt.xlabel('X')
 plt.ylabel('Y')
-
+plt.show()
+##
 # For Clustering
 from sklearn.cluster import KMeans
 
@@ -39,12 +33,11 @@ plt.scatter(centers[:, 0], centers[:, 1], c='black', s=200, alpha=0.5);
 plt.title('Displaying Cluster Centers')
 plt.xlabel('X')
 plt.ylabel('Y')
-
+##
 print(f'cluster centers are : {kmeans.cluster_centers_}')
-
+##
 # ## Let's apply it on kaggle dataset 
 # [dataset link](https://www.kaggle.com/datasets/samuelcortinhas/2d-clustering-data?resource=download)
-
 
 # Load data 
 data = pd.read_csv('Assets/data.csv')
@@ -52,20 +45,20 @@ print(f'data head is : {data.head()}')
 
 X = data[['x', 'y']]
 y = data['y']
-
+##
 X_numpy = np.array(X)
 plt.scatter(X_numpy[:, 0], X_numpy[:, 1], s=50)
 plt.title('Displaying Data')
 plt.xlabel('X')
 plt.ylabel('Y')
-
+##
 # For Clustering
 from sklearn.cluster import KMeans
 
 kmeans = KMeans(n_clusters=3)
 kmeans.fit(X)
 y_kmeans = kmeans.predict(X)
-
+##
 plt.scatter(X_numpy[:, 0], X_numpy[:, 1], c=y_kmeans, s=50, cmap='viridis')
 
 centers = kmeans.cluster_centers_
@@ -73,15 +66,9 @@ plt.scatter(centers[:, 0], centers[:, 1], c='black', s=200, alpha=0.5);
 plt.title('Displaying Cluster Centers')
 plt.xlabel('X')
 plt.ylabel('Y')
-
-
-
-# -- Sheet 2 --
-
+##
 # # KNN
 # ## Apply KNN and Make Predictions
-
-
 import numpy as np
 import pandas as pd
 import seaborn as sns
@@ -104,7 +91,6 @@ plt.ylabel('Y')
 # For Clustering
 from sklearn.cluster import KMeans
 from sklearn.neighbors import NearestNeighbors
-
 
 knn = NearestNeighbors(n_neighbors=4,
                        n_jobs=-1)
@@ -149,6 +135,3 @@ plt.scatter(centers[:, 0], centers[:, 1], c='black', s=200, alpha=0.5);
 plt.title('Displaying Cluster Centers')
 plt.xlabel('X')
 plt.ylabel('Y')
-
-
-
